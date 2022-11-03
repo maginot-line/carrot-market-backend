e7a2b755705f7035a9fc338dd55f577fb65b00e5
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-03 09:46:46 +0900

    5.2 Custom Fields

--- users/models.py
+++ users/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+    name = models.CharField(max_length=20, blank=True)
+    avatar = models.FileField(blank=True)
